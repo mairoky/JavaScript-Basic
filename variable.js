613a5d5edc5f072b72387cae67db3381d09732f4
Aminul Islam Rocky <mairoky@outlook.com>
2022-09-10 12:45:47 +0600

    explore variable

---
+++ variable.js
@@ -0,0 +1,35 @@
+// variable declaration
+/*
+Data Type:
+    Primitive
+        String
+        Number
+        Boolean
+        Undefined
+        Null
+    Non-pritive
+        Array
+        Object
+*/
+// string
+var fullName = 'Mark Rieon';
+console.log(fullName);
+// number
+var age = 30;
+console.log(age);
+// boolean
+var isStudent = false;
+console.log(isStudent);
+// undefined
+var job;
+console.log(job);
+job = 'Full Stack Developer';
+console.log(job);
+
+/*
+Variable Naming Convension
+    1. can’t use a number as the first character, first character must be a letter or an underscore (_)
+    2. can’t use any other characters, including spaces, symbols, and punctuation marks
+    3. variable names are case sensitive
+    4. can’t use one of JavaScript’s reserved words as a variable name
+*/
