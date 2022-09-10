a034c72bd211545530e57293e030f10e1d243c78
Aminul Islam Rocky <mairoky@outlook.com>
2022-09-10 13:32:09 +0600

    operator

---
+++ operators.js
@@ -0,0 +1,12 @@
+// Math Operators
+var numOne = 20;
+var numTwo = 10;
+var sum = numOne + numTwo;
+console.log(sum);
+// Logical Operator
+var isNumOneSmaller = numOne < numTwo;
+console.log(isNumOneSmaller);
+// Typeof Operator
+console.log(typeof numOne);
+console.log(typeof isNumOneSmaller);
+// Operator Precedence
